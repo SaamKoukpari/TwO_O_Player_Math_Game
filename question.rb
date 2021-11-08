a861d965e12e2f84fdb87304cfb76ec4060d8dae
Saam Koukpari <saam.koukpari@gmail.com>
2021-11-08 11:06:18 -0800

    implement logic for generating random math sum question

--- question.rb
+++ question.rb
@@ -1,5 +1,17 @@
 class Question
 
-  
+  def initialize
+    @num1 = rand(1...20)
+    @num2 = rand(1...20)
+    @sum = @num1 + @num2
+  end 
+
+  def ask(name)
+    puts "#{name}: What does #{@num1} plus #{@num2} equal"
+  end 
+
+  def check(input)
+    @sum == input
+  end 
 
 end
