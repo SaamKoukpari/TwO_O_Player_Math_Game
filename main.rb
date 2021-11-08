c9f90e83dba1b99ff7abe5ad6b1a6972fb5d49af
Saam Koukpari <saam.koukpari@gmail.com>
2021-11-08 12:35:50 -0800

    final commit

--- main.rb
+++ main.rb
@@ -2,4 +2,12 @@ require './game'
 require './player'
 require './question'
 
+puts "Player 1, please enter your name"
+player1 = $stdin.gets.chomp
 
+puts "Player 2, please enter your name"
+player2 = $stdin.gets.chomp
+
+new_game = Game.new(player1, player2)
+
+new_game.start
